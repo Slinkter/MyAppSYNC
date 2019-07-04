090368927326bf3647ad8023dc38e95106e143d2
Slinkter <luis.j.cueva@gmail.com>
2019-07-04 14:53:40 -0500

    parte 06

---
+++ app/src/main/java/com/vidrieriachaloreyes/mysqlsycn/NetworkMonitor.java
@@ -0,0 +1,12 @@
+package com.vidrieriachaloreyes.mysqlsycn;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NetworkMonitor extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
