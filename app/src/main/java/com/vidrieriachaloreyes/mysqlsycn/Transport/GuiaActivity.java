2cbf3a9d4cb8e4e299627c7bbfe210afac4db8b4
Slinkter <luis.j.cueva@gmail.com>
2019-07-10 12:39:10 -0500

    manifests

---
+++ app/src/main/java/com/vidrieriachaloreyes/mysqlsycn/Transport/GuiaActivity.java
@@ -0,0 +1,15 @@
+package com.vidrieriachaloreyes.mysqlsycn.Transport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vidrieriachaloreyes.mysqlsycn.R;
+
+public class GuiaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guia);
+    }
+}
