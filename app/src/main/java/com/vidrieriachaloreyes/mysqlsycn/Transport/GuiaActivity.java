e626d5f824f505b8c9d60539c8305d7b6635ada2
Slinkter <luis.j.cueva@gmail.com>
2019-07-10 12:42:48 -0500

    funciona bien el xml

--- app/src/main/java/com/vidrieriachaloreyes/mysqlsycn/Transport/GuiaActivity.java
+++ app/src/main/java/com/vidrieriachaloreyes/mysqlsycn/Transport/GuiaActivity.java
@@ -10,6 +10,8 @@ public class GuiaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_guia);
     }
 }
